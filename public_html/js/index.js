e1176dbc4f976a15690ae3894d1318c2a41b83b9
YOUR_NAME <YOUR_EMAIL>
2024-11-19 12:14:16 -0700

    example no db

--- public_html/js/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * File: index.js
- * Desc: JavaScript for main page of CSC 337 Final Project 
- */
-
-// TODO
--- server.js
+++ server.js
@@ -1,13 +1,44 @@
-const express = require('express');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
-const port = 3000;
-const host = "localhost"
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(express.json());
-app.use(express.static("public_html"));
+app.use(express.static("public"));
 
-// TODO add routes
+// Store active rooms and their users
+const rooms = {};
 
-app.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+io.on("connection", (socket) => {
+    socket.on("joinRoom", ({ username, room }) => {
+        socket.join(room);
+
+        // Add user to room's user list
+        if (!rooms[room]) rooms[room] = [];
+        rooms[room].push({ id: socket.id, username });
+
+        // Notify all users in the room
+        io.to(room).emit("updateUsers", rooms[room]);
+
+        // Notify the room about the new user
+        socket.to(room).emit("message", `${username} has joined the room!`);
+
+        // Listen for messages from the user
+        socket.on("chatMessage", (message) => {
+            io.to(room).emit("message", `${username}: ${message}`);
+        });
+
+        // Handle user disconnect
+        socket.on("disconnect", () => {
+            rooms[room] = rooms[room].filter((user) => user.id !== socket.id);
+            io.to(room).emit("updateUsers", rooms[room]);
+            socket.to(room).emit("message", `${username} has left the room.`);
+        });
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
 });
