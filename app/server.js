e7226263aaf67be2ede985fc4d8f3e435ad2d172
Sneupi <gabevenegas527@gmail.com>
2024-11-23 22:03:09 -0700

    base mongo

---
+++ app/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!'); //FIXME real code
+});
+
+// Start server
+mongoose.connect("mongodb://localhost/chatapp", { serverSelectionTimeoutMS: 5000 })
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}/`);
+        });
+    })
+    .catch((err) => {
+        console.error("Database init fail:", err);
+    });
