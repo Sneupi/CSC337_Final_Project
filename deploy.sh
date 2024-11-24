0a67e271690bba3a93355a0d4c53f36c70ee8984
Sneupi <gabevenegas527@gmail.com>
2024-11-24 14:23:23 -0700

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+sudo apt-get update
+
+# Check if Node.js is installed
+if ! command -v node &> /dev/null
+then
+    echo "Node.js not found, installing..."
+    sudo apt install nodejs -y
+fi
+
+# Check if npm is installed
+if ! command -v npm &> /dev/null
+then
+    echo "npm not found, installing..."
+    sudo apt install npm -y
+fi
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null
+then
+    echo "Docker not found, installing..."
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh ./get-docker.sh -y
+    sudo apt-get install docker-compose-plugin -y
+fi
+
+# Setup App 
+mkdir -p ~/app
+cd ~/app
+if [ ! -d "CSC337_Final_Project" ]; then
+    git clone https://github.com/Sneupi/CSC337_Final_Project.git
+fi
+cd CSC337_Final_Project
+git checkout exp2
+git pull
+
+# Build and run the app
+docker compose up -d --build
