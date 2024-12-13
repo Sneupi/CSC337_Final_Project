20e864d9f26720d97a226b86e2cdd0842a3e9733
Sneupi <gabevenegas527@gmail.com>
2024-12-13 16:24:08 -0700

    logout func for chatinstance

--- server/public/js/instance.js
+++ server/public/js/instance.js
@@ -154,3 +154,23 @@ document.getElementById("roomButton").addEventListener("click", function(e){
     createRoomReq.open("POST", 'http://localhost:3000/api/rooms');
     createRoomsReq.send(document.getElementById("newRoom").value);
 });
+
+//Logs user out and returns home if successful
+const logoutReq = new XMLHttpRequest();
+logoutReq.onreadystatechange = () => {
+    if (logoutReq.readyState != 4){
+        return;
+    }
+    if(logoutReq.status == 404){
+        console.log("404: ");
+        window.alert("Error logging out");
+    }else{
+        window.alert("Log out successful");
+        window.location = "./index.html";
+    }
+}
+document.getElementById("logOut").addEventListener("click", function(e){
+    console.log("Attempting log out");
+    logoutReq.open("POST", "http://localhost:3000/api/logout");
+    logoutReq.send();
+});
